c40edcb305578989f7c76460b880717fda998e7b
KIM SANGIL <fulu1268@gmail.com>
2025-12-04 01:13:01 +0900

    run_synthesis.tcl,run_implementation.tcl,generate_bitstream.tcl 추가

---
+++ generate_bitstream.tcl
@@ -0,0 +1,10 @@
+open_proj xilinx_fifo_test.xpr
+set run_name impl_1
+set cpu_count 4
+launch_runs $run_name -to_step write_bitstream -jobs $cpu_count
+wait_on_run $run_name
+set status [get_property STATUS [get_runs $run_name]]
+if {$status != "write_bitstream Complete!"} {
+exit 1
+}
+exit 0
